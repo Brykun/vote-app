05db81902bde3dbdde77e5da1282685b1dab0a85
Brykun <brikunvadim@gmail.com>
2025-06-03 12:54:31 +0300

    Виконано 5 завдання

--- vote-client/src/App.tsx
+++ vote-client/src/App.tsx
@@ -2,14 +2,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import PollPage from './pages/PollPage';
 import NewPollPage from './pages/NewPollPage'; 
+import EditPollPage from './pages/EditPollPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/polls/new" element={<NewPollPage />} /> 
         <Route path="/poll/:id" element={<PollPage />} />
-        <Route path="/polls/new" element={<NewPollPage />} />  {/* сюда */}
+        <Route path="/poll/:id/edit" element={<EditPollPage />} />
+        <Route path="/" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   );
